a8fb602cc72ed3712bebb27558383e08957a4701
Peter Drake <drake@lclark.edu>
2017-05-18 14:51:50 -0700

    Cleaned up simplify-mask.py.

--- simplify-mask.py
+++ simplify-mask.py
@@ -6,35 +6,21 @@ Created on Thu May 18 10:23:23 2017
 @author: drake
 """
 
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 from scipy import misc
 
-#img = mpimg.imread('Shades_of_green.png')
-#img = mpimg.imread('search-3-512.jpg')
-img = misc.imread('Stripes.png')
-imgplot = plt.imshow(img)
-plt.show()
+def list_colors(img):
+    """Returns an array of the unique colors in img."""
+    # Adapted from http://stackoverflow.com/questions/16970982/find-unique-rows-in-numpy-array
+    # Flatten img
+    a = img.reshape(-1, img.shape[2])
+    # Make this an array of colors (each of which is a length 4 array)
+    b = np.ascontiguousarray(a) \
+                    .view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
+    # Find the locations of the unique colors
+    _, idx = np.unique(b, return_index = True)
+    # Return those colors
+    return a[idx]
 
-def find_colors():
-    global img
-    
-    w,y,z = np.shape(img)
-    
-    a = img.reshape(y*w,z)
-    print (np.shape(a))
-    print (a)
-    """
-    new_img = np.array([tuple(row) for row in new_img])
-    print (np.shape(new_img))
-    colors = np.unique(new_img)
-    #colors = set( tuple(v) for c in img for v in c )
-    print (np.shape(new_img))
-"""
-    b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
-    _, idx = np.unique(b, return_index=True)
-
-    unique_a = a[idx]
- 
-    return unique_a
+img = misc.imread('stripes.png')
+print(list_colors(img))
