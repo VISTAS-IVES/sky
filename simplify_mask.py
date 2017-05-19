4bd3af3dfa0d34d76fba66887fa017d8aae42bf1
Jeff Mullins <jeffmullins@lclark.edu>
2017-05-19 13:06:56 -0700

    Added simplify_images method, which runs a group of images through simplify_colors() given the path

--- simplify_mask.py
+++ simplify_mask.py
@@ -5,9 +5,13 @@ Created on Thu May 18 10:23:23 2017
 
 @author: drake
 """
-
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 from scipy import misc
+import os
+from PIL import Image
+
 
 def list_colors(img):
     """Returns an array of the unique colors in img."""
@@ -30,22 +34,23 @@ GRAY = np.array([192, 192, 192])
 YELLOW = np.array([255, 255, 0])
 WHITE = np.array([255, 255, 255])
 
-
 def simplify_colors(img):
     """Makes a mask of the image to find GREEN values, then removes them"""
     mask = (img == GREEN).all(axis = 2)
     img[mask == True] = BLACK
-    
     """Does the same for YELLOW values"""
     mask = (img == YELLOW).all(axis = 2)
     img[mask == True] = BLACK
-  
-    print (img)
     return img
     
-
-
+def simplify_images(path):
+    dir_name = "simple_masks/"
+    os.mkdir(dir_name)
+    for file in os.listdir(path):
+        img = misc.imread(path + file)
+        new_img = simplify_colors(img)
+        im = Image.fromarray(new_img)
+        im.save(dir_name + file)
     
 if __name__ == '__main__':
-    img = misc.imread('data/masks/sgptsicldmaskC1.a1.20160414.173800.png.20160414173800.png')
-    print(list_colors(img))
+    path = ("data/masks/")
