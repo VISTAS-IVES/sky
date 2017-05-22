1e733658e7b2ca2c1a321705f575abcc6a7e7bcd
Jeff Mullins <jeffmullins@lclark.edu>
2017-05-22 13:49:14 -0700

    Got Tensor_flow_practice.py running without errors. (Does not actually learn yet)

--- Tensor_flow_practice.py
+++ Tensor_flow_practice.py
@@ -3,20 +3,20 @@
 """
 Created on Mon May 22 10:20:00 2017
 
-@author: jeffmullins
+@author: Jeff Mullins, Sean Richardson
 """
 
-
-import argparse
-import sys
-import numpy as np
 import tensorflow as tf
 
 def main():
-    #data
-    inputs = np.array([[0,0,255],[255,255,255],[0,0,0],[0,0,255]])
+
+    inputs = [[0,0,255],[255,255,255],[0,0,0],[0,0,255]]
     # if blue out is 0,0,1 if white 1,0,0 if black 0,1,0
+    
     f = tf.constant(1.0/255.0)
     i = tf.placeholder(tf.float32, [4,3])
-    o = tf.placeholder(tf.float32, tf.mul(i, f))
+    o = tf.multiply(i, f)
     
+    sess = tf.Session()
+    result = sess.run(o, feed_dict={i: inputs})
+    print (result)
