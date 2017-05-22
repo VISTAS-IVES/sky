86f2e13ca154e6031b624d36c423b1010054d3cf
Peter Drake <drake@lclark.edu>
2017-05-22 15:34:57 -0700

    Adapted learning code from TensorFlow MNIST softmax tutorial.

--- Tensor_flow_practice.py
+++ Tensor_flow_practice.py
@@ -9,18 +9,23 @@ Created on Mon May 22 10:20:00 2017
 import tensorflow as tf
     
 if __name__ == '__main__':
-#    inputs = [[0,0,255],[255,255,255],[0,0,0],[0,0,255]]
-#    correct = [[0,0,1], [1,1,1], [0,0,0], [0,0,1]]
     inputs = [[0,0,255],[0,255,0], [0, 255, 255]]
-    correct = [[0,0,1], [0,1,0], [0, 0.5, 0.5]]
+    correct = [[1, 0, 0], [0,1,0], [0, 0, 1]]
     # if blue out is 0,0,1 if white 1,0,0 if black 0,1,0
     # Define the network
     x = tf.placeholder(tf.float32, [None, 3])
-    y = tf.nn.softmax(x)
+    W = tf.Variable(tf.zeros([3, 3]))
+    b = tf.Variable(tf.zeros([3]))
+    y = tf.matmul(x, W) + b
     y_ = tf.placeholder(tf.float32, [None, 3])
     cross_entropy = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=x))
-    # Run it
+      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    # Train
+    init = tf.global_variables_initializer()
     sess = tf.Session()
+    sess.run(init)
+    for _ in range(1000):
+        sess.run(train_step, feed_dict={x: inputs, y_: correct})
+    # Test
     print(sess.run(y, feed_dict={x: inputs}))
-    print(sess.run(cross_entropy, feed_dict={x: inputs, y_: correct}))
