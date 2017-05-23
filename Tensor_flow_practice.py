116276a6a5388c784a379fd5f1d62a54ffe73420
Jeff Mullins <jeffmullins@lclark.edu>
2017-05-23 09:11:45 -0700

    Added Mask_to_one_hot function

--- Tensor_flow_practice.py
+++ Tensor_flow_practice.py
@@ -5,12 +5,35 @@ Created on Mon May 22 10:20:00 2017
 
 @author: Jeff Mullins, Sean Richardson
 """
-
+import numpy as np
+from scipy import misc
+import os
+from PIL import Image
 import tensorflow as tf
     
+    
+def mask_to_one_hot(img):
+    BLACK = np.array([0, 0, 0])
+    BLUE = np.array([0, 0, 255])
+    WHITE = np.array([255, 255, 255])
+
+    img[(img == WHITE).all(axis = 2)] = [1,0,0]
+    img[(img == BLUE).all(axis = 2)]  = [0,1,0]
+    img[(img == BLACK).all(axis = 2)] = [0,0,1]
+    return img
+  
+
 if __name__ == '__main__':
-    inputs = [[0,0,255],[0,255,0], [0, 255, 255]]
-    correct = [[1, 0, 0], [0,1,0], [0, 0, 1]]
+    #get image and make the mask into a one-hotted mask
+    inputs = misc.imread('data/images/sgptsiskyimageC1.a1.20160414.162830.jpg.20160414162830.jpg')
+    correct = mask_to_one_hot(misc.imread('data/simplified_masks/sgptsicldmaskC1.a1.20160414.162830.png.20160414162830.png'))
+
+    
+    #flatten the images and masks
+    inputs = inputs.reshape(-1,3)
+    correct = correct.reshape(-1,3)
+    
+    
     # if blue out is 0,0,1 if white 1,0,0 if black 0,1,0
     # Define the network
     x = tf.placeholder(tf.float32, [None, 3])
