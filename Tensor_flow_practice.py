50788897cddbe5164fd59a52b63b9af7070de0b4
Jeff Mullins <jeffmullins@lclark.edu>
2017-05-23 17:10:04 -0700

    Got it to print resulting guess cast (not very well)

--- Tensor_flow_practice.py
+++ Tensor_flow_practice.py
@@ -19,17 +19,23 @@ BLUE = np.array([0, 0, 255])
 WHITE = np.array([255, 255, 255])  
     
 def mask_to_one_hot(img):
-    img[(img == WHITE).all(axis = 2)] = [1,0,0]
-    img[(img == BLUE).all(axis = 2)]  = [0,1,0]
-    img[(img == BLACK).all(axis = 2)] = [0,0,1]
+    img[(img == WHITE).all(axis = 2)] = np.array([1,0,0])
+    img[(img == BLUE).all(axis = 2)]  = np.array([0,1,0])
+    img[(img == BLACK).all(axis = 2)] = np.array([0,0,1])
     
     return img
 
 def one_hot_to_mask(img):
-    img[(img == [1,0,0]).all(axis = 2)] = WHITE
-    img[(img == [0,1,0]).all(axis = 2)] = BLUE
-    img[(img == [0,0,1]).all(axis = 2)] = BLACK
+    img[(img == np.array([1,0,0])).all(axis = 2)] = WHITE
+    img[(img == np.array([0,1,0])).all(axis = 2)] = BLUE
+    img[(img == np.array([0,0,1])).all(axis = 2)] = BLACK
     return img
+
+def out_to_image(output):
+    #takes the output of one-hot vectors and converts it into an RGB image
+    output = output.reshape([480,480,3])
+    output = one_hot_to_mask(output)
+    return output
 
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
@@ -45,7 +51,12 @@ def conv2d(x, W):
 if __name__ == '__main__':
     # Get image and make the mask into a one-hotted mask
     inputs = misc.imread('data/simplified_images/sgptsiskyimageC1.a1.20160414.162830.jpg.20160414162830.jpg')
+    print(inputs.shape)
     correct = mask_to_one_hot(misc.imread('data/simplified_masks/sgptsicldmaskC1.a1.20160414.162830.png.20160414162830.png'))
+   # correct = misc.imread('data/simplified_masks/sgptsicldmaskC1.a1.20160414.162830.png.20160414162830.png')
+
+    #Image.fromarray(correct, 'RGB').show()
+
     inputs = inputs.reshape(1, 480, 480, 3)
     correct = correct.reshape(-1, 3)
     # Define the network
@@ -55,6 +66,8 @@ if __name__ == '__main__':
     h = tf.nn.relu(conv2d(x, W) + b)
     y = tf.reshape(h, [-1, 3])
     y_ = tf.placeholder(tf.float32, [None, 3])
+    out = tf.nn.softmax(y)
+    
     # Is this minimizing across every output channel, pixel, and image?
     # Is that what we want?
     cross_entropy = tf.reduce_mean(
@@ -66,7 +79,7 @@ if __name__ == '__main__':
     # Train
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-    for i in range(20000):
+    for i in range(1000):
 #        batch = mnist.train.next_batch(50)
         if i%100 == 0:
             train_accuracy = sess.run(accuracy, feed_dict={
@@ -75,6 +88,20 @@ if __name__ == '__main__':
 #                    x:batch[0], y_: batch[1]})
             print("step %d, training accuracy %g"%(i, train_accuracy))
         sess.run(train_step, feed_dict={x: inputs, y_: correct})
+
+    output =  (sess.run(out, feed_dict={x: inputs}))
+    print (type(output))
+    output = output.astype(int)
+    output = out_to_image(output)
+    print (type(output))
+    #print (output.shape)
+    #int_output = output.astype(int)
+    #print (int_output)
+    #print (np.where(output > 255))
+    Image.fromarray(output.astype('uint8')).show()
+
+    #Image.fromarray(output).show()
+    
 #        train_step.run(feed_dict={x: batch[0], y_: batch[1]})
     # Test    
 #    accuracy = sess.run(accuracy, feed_dict={
