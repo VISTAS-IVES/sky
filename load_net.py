f25b41c6a8d5a52a2281f090828ceff6cdef7d4f
Peter Drake <drake@Peters-MacBook-Pro.local>
2017-06-15 08:25:01 -0700

    Modified load_net.py to look in results/test3.

--- load_net.py
+++ load_net.py
@@ -41,4 +41,4 @@ def load_net(train_step, accuracy, saver, init, x, y, y_, cross_entropy, num, re
 #        img.save('results/out2.png')
 
 if __name__ == '__main__':
-    load_net(*build_net(), sys.argv[1], 'results/job_number_2_learning_rate_0.0001_layer_sizes_32_32_20170614144633/')
+    load_net(*build_net(), sys.argv[1], 'results/test3/')
