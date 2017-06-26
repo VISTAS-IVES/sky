aeac38475399c5caf14006f130ff9ff4a38d5a45
Peter Drake <drake@lclark.edu>
2017-06-26 09:59:47 -0700

    Exp 26: a closer look at early minibatches.

--- net.py
+++ net.py
@@ -222,13 +222,13 @@ def train_net(train_step, accuracy, saver, init, x, y, y_, ns, cross_entropy,
         with tf.Session() as sess:
             init.run()
             print('Step\tTrain\tValid', file=f, flush=True)
-            for i in range(1, 5000 + 1):
+            for i in range(1, 100 + 1):
                 batch = random.sample(train_stamps, BATCH_SIZE)
                 inputs = get_inputs(batch)
                 correct = get_masks(batch)
                 ns_vals = get_nsmasks(batch)
                 train_step.run(feed_dict={x: inputs, y_: correct, ns: ns_vals})
-                if i % 100 == 0:
+                if i % 1 == 0:
                     saver.save(sess, result_dir + 'weights', global_step=i)
                     train_accuracy = accuracy.eval(feed_dict={
                             x: inputs, y_: correct, ns: ns_vals})
