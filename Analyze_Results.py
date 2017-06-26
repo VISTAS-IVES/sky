1c453778e3c3e02296a7303dd184f374ac4f2273
Peter Drake <drake@lclark.edu>
2017-06-26 16:02:09 -0700

    Working on cleaning up Analyze_Results.py.

--- Analyze_Results.py
+++ Analyze_Results.py
@@ -10,88 +10,91 @@ Created on Thu Jun 15 15:32:13 2017
 @author: jeffmullins
 """
 
-from net import build_net, get_inputs, format_nsmask, get_nsmasks
+from net import build_net, get_inputs, get_nsmasks
 from load_net import out_to_image
 import numpy as np
 import sys
 import tensorflow as tf
 from PIL import Image
-import matplotlib.pyplot as plt
 from scipy import misc
-import os
 import pickle
+from net import BATCH_SIZE
 
 BLUE = np.array([0, 0, 255])
 WHITE = np.array([255, 255, 255])
 GRAY = np.array([192, 192, 192])
 
-#in format: "what we picked"_TO_"the correct answer"
-BLUE_TO_GRAY = [85, 0 , 0]
-BLUE_TO_WHITE = [170, 0, 0]
-GRAY_TO_BLUE = [0, 85, 0]
-GRAY_TO_WHITE = [255, 0, 0]
-WHITE_TO_BLUE = [0, 170, 0]
-WHITE_TO_GRAY = [0, 255, 0]
-#def make_compared_image(result, mask):
-#    mask[(result != mask).any(axis=2)] = [255, 0 , 0]
-#    mask = Image.fromarray(mask.astype('uint8'))
-#    mask.show()
-#    return mask
+# BLUE_FOR_GRAY (for example) means our net gave blue when the target mask
+# gave gray
+BLUE_FOR_GRAY = [85, 0, 0]  # Very dark red
+BLUE_FOR_WHITE = [170, 0, 0]  # Dark red
+GRAY_FOR_WHITE = [255, 0, 0]  # Bright red
+GRAY_FOR_BLUE = [0, 85, 0]  # Dark green
+WHITE_FOR_BLUE = [0, 170, 0]  # Medium green
+WHITE_FOR_GRAY = [0, 255, 0]  # Bright green
 
 
-def read_in(directory):
-    F = open(directory + 'parameters.txt',"r")
+def read_parameters(directory):
+    """Reads the parameters.txt file in directory. Returns a dictionary
+    associating labels with keys."""
+    F = open(directory + 'parameters.txt', 'r')
     file = F.readlines()
-    args = []
+    args = {}
     for line in file:
-        args.append(line[line.index("\t")+len("\t"):line.index("\n")])
+        key, value = line.split(':\t')
+        args[key] = value
     return args
 
-def get_recent_step_version(directory):
-    F = open(directory + 'output.txt',"r")
+
+def read_last_iteration_number(directory):
+    """Reads the output.txt file in directory. Returns the iteration number
+    on the last row."""
+    F = open(directory + 'output.txt', 'r')
     file = F.readlines()
-    line = file[len(file)-1]
+    line = file[len(file) - 1]
     return (line.split()[0])
 
 
-def make_compared_images(results, masks):
-    for i in range(len(results)):
-        masks[i][np.logical_and((results[i] == BLUE).all(axis=2), (masks[i] == GRAY).all(axis=2))] = BLUE_TO_GRAY
-        masks[i][np.logical_and((results[i] == BLUE).all(axis=2), (masks[i] == WHITE).all(axis=2))] = BLUE_TO_WHITE
-        masks[i][np.logical_and((results[i] == GRAY).all(axis=2), (masks[i] == BLUE).all(axis=2))] = GRAY_TO_BLUE
-        masks[i][np.logical_and((results[i] == GRAY).all(axis=2), (masks[i] == WHITE).all(axis=2))] = GRAY_TO_WHITE
-        masks[i][np.logical_and((results[i] == WHITE).all(axis=2), (masks[i] == BLUE).all(axis=2))] = WHITE_TO_BLUE
-        masks[i][np.logical_and((results[i] == WHITE).all(axis=2), (masks[i] == GRAY).all(axis=2))] = WHITE_TO_GRAY
-        disp = Image.fromarray(masks[i].astype('uint8'))
+def show_comparison_images(outputs, targets):
+    """Shows images of where outputs differ targets, color-coded by how they
+    agree or disagree. Destructively modifies targets."""
+    for i in range(len(outputs)):
+        targets[i][np.logical_and((outputs[i] == BLUE).all(axis=2), (targets[i] == GRAY).all(axis=2))] = BLUE_FOR_GRAY
+        targets[i][np.logical_and((outputs[i] == BLUE).all(axis=2), (targets[i] == WHITE).all(axis=2))] = BLUE_FOR_WHITE
+        targets[i][np.logical_and((outputs[i] == GRAY).all(axis=2), (targets[i] == BLUE).all(axis=2))] = GRAY_FOR_BLUE
+        targets[i][np.logical_and((outputs[i] == GRAY).all(axis=2), (targets[i] == WHITE).all(axis=2))] = GRAY_FOR_WHITE
+        targets[i][np.logical_and((outputs[i] == WHITE).all(axis=2), (targets[i] == BLUE).all(axis=2))] = WHITE_FOR_BLUE
+        targets[i][np.logical_and((outputs[i] == WHITE).all(axis=2), (targets[i] == GRAY).all(axis=2))] = WHITE_FOR_GRAY
+        disp = Image.fromarray(targets[i].astype('uint8'))
         disp.show()
-    return masks
 
 
-#def make_compared_images(results, masks):
-#    for i in range(len(results)):
-#        masks[i][(results[i] != masks[i]).any(axis=2)] = [255, 0 , 0]
-#        disp = Image.fromarray(masks[i].astype('uint8'))
-#        disp.show()
-#    return masks
-
-def get_valid_stamps():
+def read_valid_stamps():
+    """Reads the valid.stamps file in data and returns a list of stamps."""
     with open('data/valid.stamps', 'rb') as f:
         valid_stamps = pickle.load(f)
-    valid_stamps = valid_stamps[:50]
+    valid_stamps = valid_stamps[:BATCH_SIZE]
     return valid_stamps
 
-def get_masks(time_stamps):
-    masks = np.empty((len(time_stamps), 480, 480, 3))
-    for i, s in enumerate(time_stamps):
-        masks[i] = np.array(misc.imread('data/simplemask/simplemask' + str(s) + '.png'))
-    return masks
-     
-def get_mask(time_stamp):
+
+def read_target(time_stamp):
+    """Reads and returns the target mask corresponding to time_stamps from
+    the simplemask directory."""
     return np.array(misc.imread('data/simplemask/simplemask' + str(time_stamp) + '.png'))
 
-   
-def find_num_disagreeing_pixels(results, mask):
-    return np.sum((results != mask).any(axis=2)) / (480*480)
+
+def read_targets(time_stamps):
+    """Reads and returns the target masks corresponding to time_stamps from
+    the simplemask directory."""
+    masks = np.empty((len(time_stamps), 480, 480, 3))
+    for i, s in enumerate(time_stamps):
+        masks[i] = read_target(s)
+    return masks
+
+
+def disagreement_rate(output, target):
+    """Returns the proportion of pixels in output that disagree with target."""
+    return np.sum((output != target).any(axis=2)) / (480*480)
 
 
 def load_stamps(train_step, accuracy, saver, init, x, y, y_, ns, cross_entropy, result_dir, num, stamps):
@@ -106,15 +109,15 @@ def find_worst_results(num_worst, time_stamps, directory, step_version, kernel, 
     train_step, accuracy, saver, init, x, y, y_, ns, cross_entropy = build_net(kernel_width = kernel, layer_sizes = layers)
     with tf.Session() as sess:
         saver.restore(sess, directory + 'weights-' + str(step_version))
-        time_stamps = get_valid_stamps()
+        time_stamps = read_valid_stamps()
         num_inconsistent = np.zeros(len(time_stamps))
         for i, s in enumerate(time_stamps):
             inputs = get_inputs([s])
             ns_vals = get_nsmasks([s])
             result = out_to_image(y.eval(feed_dict={x: inputs, ns: ns_vals}))
             result = result.reshape(480, 480, 3)
-            mask = get_mask(s)
-            num_inconsistent[i] = find_num_disagreeing_pixels(result, mask)
+            mask = read_target(s)
+            num_inconsistent[i] = disagreement_rate(result, mask)
         indices = num_inconsistent.argsort()[num_worst*-1:][::-1]
         print ("Worst results percentages:\t" + str(np.take(num_inconsistent, indices)))
     return np.take(time_stamps, indices)
@@ -126,16 +129,14 @@ def display_sky_images(time_stamps):
 
 
 if __name__ == '__main__':
-    
-    time_stamps = get_valid_stamps()
+    time_stamps = read_valid_stamps()
     dir_name = "results/" + sys.argv[1] + "/"
-    args = read_in(dir_name)
-    step_version = get_recent_step_version(dir_name)
-    kernel_width = int(args[2])
-    layer_sizes = list(map(int,args[3].split()))
-    
+    args = read_parameters(dir_name)
+    step_version = read_last_iteration_number(dir_name)
+    kernel_width = int(args['Kernel width'])
+    layer_sizes = list(map(int, args['Layer sizes'].split()))
     worst_time_stamps = find_worst_results(5, time_stamps, dir_name, step_version, kernel_width, layer_sizes)
     print ("Worst time stamps:\t" + str(worst_time_stamps))
-    results = load_stamps(*build_net(0, kernel_width, layer_sizes), dir_name, step_version, worst_time_stamps)
-    masks = get_masks(worst_time_stamps)
-    make_compared_images(results, masks)
+    outputs = load_stamps(*build_net(0, kernel_width, layer_sizes), dir_name, step_version, worst_time_stamps)
+    targets = read_targets(worst_time_stamps)
+    show_comparison_images(outputs, targets)
