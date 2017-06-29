32cf703c351c12beafa610b9cdeeb6fd625ff593
Peter Drake <drake@lclark.edu>
2017-06-28 17:55:10 -0700

    Moved kludge code into preprocess.py

--- kludge.py
+++ kludge.py
@@ -9,7 +9,7 @@ Created on Wed Jun 28 12:54:34 2017
 import os
 from preprocess import extract_timestamp
 
-for file in os.listdir('data/cldmask/'):
+for file in os.listdir('cldmask/'):
     t = extract_timestamp(file)
-    if not os.path.isfile('data/simpleimage/simpleimage' + t + '.jpg'):
+    if not os.path.isfile('simpleimage/simpleimage' + t + '.jpg'):
         print('There is no simple image for ' + t)
