ffd7c5f34006110ad5a6e1f857f51633b907e778
Peter Drake <drake@lclark.edu>
2017-06-30 09:35:30 -0700

    Exp 32, zooming in on the first 50 minibatches.

--- train.py
+++ train.py
@@ -224,23 +224,21 @@ def train_net(train_step, accuracy, saver, init, x, y, y_, ns, cross_entropy,
         with tf.Session() as sess:
             init.run()
             print('Step\tTrain\tValid', file=f, flush=True)
-            for i in range(1, 1000 + 1):
+            for i in range(1, 50 + 1):
                 batch = random.sample(train_stamps, BATCH_SIZE)
                 inputs = get_inputs(batch)
                 correct = get_masks(batch)
                 ns_vals = get_nsmasks(batch)
                 train_step.run(feed_dict={x: inputs, y_: correct, ns: ns_vals})
-                if i % 50 == 0:
+                if i % 1 == 0:
                     saver.save(sess, result_dir + 'weights', global_step=i)
                     train_accuracy = accuracy.eval(feed_dict={
                             x: inputs, y_: correct, ns: ns_vals})
                     valid_accuracy = accuracy.eval(feed_dict={
                             x: valid_inputs, y_: valid_correct, ns: valid_ns_vals})
-
-                    print('{}\t{:1.5f}\t{:1.5f}'.format(i, train_accuracy, valid_accuracy), file=f, flush=True)                             
-            
-        stop = time.time()  
-        F = open(out_dir + 'parameters.txt',"a")
+                    print('{}\t{:1.5f}\t{:1.5f}'.format(i, train_accuracy, valid_accuracy), file=f, flush=True)            
+        stop = time.time()
+        F = open(out_dir + 'parameters.txt', 'a')
         F.write("Elapsed time:\t" + str(stop - start) + " seconds\n")
         F.close()
 
