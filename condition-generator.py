510b1f33214fbb38c5089094cf080dfec60d29cd
Peter Drake <drake@lclark.edu>
2017-07-21 09:35:48 -0700

    Exp 45: shortcut connections.

--- condition-generator.py
+++ condition-generator.py
@@ -6,15 +6,16 @@ Created on Wed Jul 19 10:04:42 2017
 @author: drake
 """
 
-exp = 44
+exp = 45
 i = 0
 variants = ('a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-5-e',
-            'a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:conv-{0}-5-f',
-            'a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:conv-{0}-{1}-f h:conv-{0}-5-g')
+            'a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:concat-a-f h:conv-{0}-5-g',
+            'a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:conv-{0}-{1}-f h:concat-a-g i:conv-{0}-5-h',
+            'a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:conv-{0}-{1}-f h:conv-{0}-{1}-g i:concat-a-h j:conv-{0}-5-i')
 
 for v in variants:
-    for kernel_width in (3, 5):
-        for channels in (64, 128):
+    for kernel_width in (5, ):
+        for channels in (16, 32):
             for j in range(2):
                 print('{}-{:0>2} '.format(exp, i) + v.format(kernel_width, channels))
                 i += 1
