fc8dfc965fd443a3bb3c964dd7f4e5400dda4f1b
Peter Drake <drake@lclark.edu>
2017-07-24 09:50:13 -0700

    Exp 46: Late skinny pools.

--- condition-generator.py
+++ condition-generator.py
@@ -6,16 +6,13 @@ Created on Wed Jul 19 10:04:42 2017
 @author: drake
 """
 
-exp = 45
+exp = 46
 i = 0
-variants = ('a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-5-e',
-            'a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:concat-a-f h:conv-{0}-5-g',
-            'a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:conv-{0}-{1}-f h:concat-a-g i:conv-{0}-5-h',
-            'a:conv-{0}-{1}-in b:maxpool-1-100-a c:maxpool-100-1-a d:concat-a-b e:concat-c-d f:conv-{0}-{1}-e g:conv-{0}-{1}-f h:conv-{0}-{1}-g i:concat-a-h j:conv-{0}-5-i')
+variants = ('a:conv-{0}-{1}-in b:conv-{0}-{1}-a c:conv-{0}-{1} d:maxpool-1-100-c e:maxpool-100-1-c f:concat-c-d g:concat-e-f h:concat-g-in i:conv-{0}-5-h',)
 
 for v in variants:
     for kernel_width in (5, ):
-        for channels in (16, 32):
-            for j in range(2):
+        for channels in (16, 32, 64):
+            for j in range(5):
                 print('{}-{:0>2} '.format(exp, i) + v.format(kernel_width, channels))
                 i += 1
