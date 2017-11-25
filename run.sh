554b4bdb3987744273bfc918b17218ecc38ec325
Peter Drake <drake@lclark.edu>
2017-11-24 17:43:57 -0800

    Test experiment: large with phi nodes.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 20
 #SBATCH --mem-per-cpu 4096
-#SBATCH --partition medium
+#SBATCH --partition phi
 #SBATCH --output=output.txt
 
 module load Python/3.6.2/intel
