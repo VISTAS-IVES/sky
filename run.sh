e22061fed215532c799c69615677ecae3aaaf7ff
Peter Drake <drake@lclark.edu>
2017-11-28 10:44:45 -0800

    Live test: phi

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name clouds
+#SBATCH --job-name cloudsphi
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 20
 #SBATCH --mem-per-cpu 4096
-#SBATCH --partition medium
+#SBATCH --partition phi
 #SBATCH --output=output.txt
 
 module load Python/3.6.2/intel
